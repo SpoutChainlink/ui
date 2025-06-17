1d0d447965de05681cb566af71ee4d52aa89b8e8
0xmihirsahu <0xmihirsahu@gmail.com>
2025-06-18 02:02:23 +0530

    loading and not found pages

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div>
+        <h1>Loading...</h1>
+        <p>The page is loading. Please wait...</p>
+    </div>
+  )
+}
+
+export default Loading;
